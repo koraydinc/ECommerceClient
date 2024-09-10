cae7559b3d4a799826892127314de9fb79a7bbb1
Koray Dinc <koray.dnc@hotmail.com>
2024-09-10 21:33:11 +0300

    Adding modules and components.

---
+++ src/app/admin/components/components.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomersModule } from './customers/customers.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CustomersModule,
+    ProductsModule,
+    OrdersModule,
+    DashboardModule
+  ],
+  exports: [
+    CustomersModule,
+    ProductsModule,
+    OrdersModule,
+    DashboardModule
+  ]
+})
+export class ComponentsModule { }
