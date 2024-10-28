ecf99cbd15821ff1e81eaad7ac85a73b5385e84f
Koray Dinc <koray.dnc@hotmail.com>
2024-10-28 19:41:19 +0300

    import componentModule to adminModule

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from './layout/layout.module';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    LayoutModule
+    LayoutModule,
+    ComponentsModule
   ],
   exports: [
   ]
