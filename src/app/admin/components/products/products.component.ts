f0e1d11429b51a22e528ad38cfc9eaec8294909a
Koray Dinc <koray.dnc@hotmail.com>
2024-11-01 20:07:34 +0300

    remove file-upload tag from products.component

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { BaseComponent, SpinnerType } from '../../../base/base.component';
+import { BaseComponent } from '../../../base/base.component';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { HttpClientService } from '../../../services/common/http-client.service';
 import { Create_Product } from '../../../contracts/create_product';
