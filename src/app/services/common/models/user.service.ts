472166668d95ba27f813a5404eb7554b2dddd645
Koray DINC <koray.dnc@hotmail.com>
2025-09-24 18:21:45 +0300

    added Token and TokenResponse classes; updated UserService to handle token login and added validation warnings in LoginComponent

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,15 +1,18 @@
+import { Token } from '../../../contracts/token/token';
 import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
 import { Create_User } from '../../../contracts/user/create_user';
 import { User } from '../../../entities/user';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
 import { HttpClientService } from '../http-client.service';
+import { TokenResponse } from '../../../contracts/token/tokenResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClientService: HttpClientService) { }
+  constructor(private httpClientService: HttpClientService, private toastrService: CustomToastrService) { }
 
   async create(user: User): Promise<Create_User> {
     const observable: Observable<Create_User | User> = this.httpClientService.post<Create_User | User>({
@@ -19,12 +22,18 @@ export class UserService {
     return await firstValueFrom(observable) as Create_User;
   }
 
-  async login(usernameOrEmail: string, password: string): Promise<void>{
-    const observable: Observable<any> = this.httpClientService.post({
+  async login(usernameOrEmail: string, password: string): Promise<void> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
       controller: 'users',
       action: 'login'
     }, { usernameOrEmail, password });
 
-    return await firstValueFrom(observable);
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    if (tokenResponse) {
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+      //localStorage.setItem("expiration", token.expiration.toString());
+
+      this.toastrService.message("Login successful", "Success", { messageType: ToastrMessageType.Success, position: ToastrPosition.TopRight });
+    }
   }
 }
--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { UserService } from '../../../services/common/models/user.service';
-import { BaseComponent, SpinnerType } from '../../../base/base.component';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from '../../../base/base.component';
+import { UserService } from '../../../services/common/models/user.service';
 
 @Component({
   selector: 'app-login',
@@ -12,10 +12,17 @@ export class LoginComponent extends BaseComponent {
   constructor(private userService: UserService, spinner : NgxSpinnerService) {
     super(spinner);
    }
-
+   showPasswordWarning = false;
+   showUsernameWarning = false;
 
   login(usernameOrEmail: string, password: string) {
     this.showSpinner(SpinnerType.BallFussion);
+    this.showPasswordWarning = !password;
+    this.showUsernameWarning = !usernameOrEmail;
+    if (this.showPasswordWarning || this.showUsernameWarning) {
+      this.hideSpinner(SpinnerType.BallFussion);
+      return;
+    }
     this.userService.login(usernameOrEmail, password)
       .then(() => {
         this.hideSpinner(SpinnerType.BallFussion);
