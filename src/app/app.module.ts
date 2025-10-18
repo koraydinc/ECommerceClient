4790cf0d9eac557afa0f7036015f67a849482d31
Koray DINC <koray.dnc@hotmail.com>
2025-10-18 15:23:43 +0300

    Upgrade Angular dependencies to version 18.x and refactor HttpClient import in AppModule for improved HTTP handling.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,32 +7,26 @@ import { UiModule } from './ui/ui.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideToastr } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AdminModule,
-    UiModule,
-    NgxSpinnerModule,
-    HttpClientModule,
-    JwtModule.forRoot({
-      config: {
-        tokenGetter: () => localStorage.getItem("accessToken"),
-        allowedDomains: ["localhost:7188"]
-      }
-    })
-  ],
-  providers: [
-    { provide: "baseUrl", useValue: "https://localhost:7188/api", multi: true },
-    provideAnimationsAsync(),
-    provideToastr()
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        AdminModule,
+        UiModule,
+        NgxSpinnerModule,
+        JwtModule.forRoot({
+            config: {
+                tokenGetter: () => localStorage.getItem("accessToken"),
+                allowedDomains: ["localhost:7188"]
+            }
+        })], providers: [
+        { provide: "baseUrl", useValue: "https://localhost:7188/api", multi: true },
+        provideAnimationsAsync(),
+        provideToastr(),
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
